88c9d6af6d29b6492ef48a1cb0e704c5c387b140
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-15 23:41:12 +0530

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Rushabh Jadhav</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Tech Stack</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <div className="social-links"></div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
