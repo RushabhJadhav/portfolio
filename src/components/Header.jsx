205739fbc2a7e848fb4279b012aa0be0a4cce6d4
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-16 11:32:52 +0530

    header styling completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 const Header = () => {
     return (
         <>
@@ -11,8 +16,18 @@ const Header = () => {
                         <li><a href="#">Projects</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
+                    <div className="social-links">
+                        <a href="https://github.com/RushabhJadhav/" target='_blank'>
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href="https://www.linkedin.com/in/rushabh-jadhav-002294249/" target='_blank'>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="https://www.instagram.com/j_rushabh39/" target='_blank'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </div>
                 </nav>
-                <div className="social-links"></div>
             </header>
         </>
     )
