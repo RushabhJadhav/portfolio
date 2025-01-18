201f7b16bc0e4fcc99446d55729bdaa2390131cb
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-19 03:30:49 +0530

    looped project cards

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 const Intro = () => {
     return (
         <div className="intro">
-            <h1>Hi, I'm Rushabh Jadhav, a Self Taught <br/> Frontend Developer</h1>
+            <h1>Hi, I'm Rushabh, a Self Taught <br/> Frontend Developer</h1>
             <span>Based in Mumbai, Maharashtra, India <br/> Specialized in Vue and React</span>
             {/* <div className="picture"></div> */}
             {/* <button>Resume</button> */}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,37 +1,53 @@
-import rectangeImage from "../assets/img/Rectangle_8.png";
+import rectangeImage1 from "../assets/img/Rectangle_1.png";
+import rectangeImage2 from "../assets/img/Rectangle_2.png";
+import rectangeImage3 from "../assets/img/Rectangle_3.png";
+import rectangeImage4 from "../assets/img/Rectangle_4.png";
+import rectangeImage5 from "../assets/img/Rectangle_5.png";
+import rectangeImage6 from "../assets/img/Rectangle_6.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 const Projects = () => {
+    const projectCards = [
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage1, preview: "", code: ""},
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage2, preview: "", code: ""},
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage3, preview: "", code: ""},
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage4, preview: "", code: ""},
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage5, preview: "", code: ""},
+        {title: "Project Tile goes here", desc: "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content", stack: "HTML , JavaScript, SASS, React", img: rectangeImage6, preview: "", code: ""},
+    ]
+
     return (
         <div className="projects-container section">
             <h1>Projects</h1>
             <p>Things I've built so far</p>
             <div className="cards-container">
-                <div className="card">
-                    <div className="card-img" style={{backgroundImage: `url(${rectangeImage})`}}>
+                {projectCards.map((item, index) => {
+                    return <div className="card" key={index}>
+                        <div className="card-img" style={{backgroundImage: `url(${item.img})`}}>
+                        </div>
+                        <div className="card-texts">
+                            <h3>{ item.title }</h3>
+                            <p>{ item.desc }</p>
+                            <p>Tech stack : <span>{ item.desc }</span></p>
+                            <ul className="card-links">
+                                <li>
+                                    <a href={item.preview} target="_blank">
+                                        <FontAwesomeIcon icon={faLink} />
+                                        Live Preview
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href={item.code} target="_blank">
+                                        <FontAwesomeIcon icon={faGithub} />
+                                        View Code
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="card-texts">
-                        <h3>Project Tile goes here</h3>
-                        <p>This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</p>
-                        <p>Tech stack : HTML , JavaScript, SASS, React</p>
-                        <ul className="card-links">
-                            <li>
-                                <a href="#" target="_blank">
-                                    <FontAwesomeIcon icon={faLink} />
-                                    Live Preview
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" target="_blank">
-                                    <FontAwesomeIcon icon={faGithub} />
-                                    View Code
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                })}
             </div>
         </div>
     )
