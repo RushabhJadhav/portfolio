1412fc79deb981a0e57fece09b08b51e73db9b06
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-21 22:21:41 +0530

    completed about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,56 @@ const About = () => {
         <div id="about-section" className="section">
             <div className="about-container">
                 <h1>About Me</h1>
-                <p>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</p>
+                <p>I have completed my Bachelor of Arts degree in Political Science and Psychology. During my final year, I discovered coding and was instantly hooked. What started as a personal interest quickly grew into a deep passion for frontend development. My self-taught journey has been fueled by curiosity, determination, and a drive to create seamless, user-friendly web experiences.</p>
+                <p>Learning to code has not only transformed my career aspirations but has also taught me the value of continuous learning and problem-solving. I am committed to honing my skills and contributing to projects that make a meaningful impact.</p>
             </div>
-            <div className="work-experience"></div>
-            <div className="education">
-                <FontAwesomeIcon icon={faLocation} />
-                <FontAwesomeIcon icon={faBuilding} />
-                <FontAwesomeIcon icon={faCalendarDays} />
+            <div className="qualifications">
+                <div className="experience-cont">
+                    <h1>Work Experience</h1>
+                    <div className="experience">
+                        <div className="upper-section">
+                            <h3>Assistant Software Consultant</h3>
+                            <span className="tenure">Full Time</span>
+                        </div>
+                        <div className="lower-section">
+                            <span>
+                                <FontAwesomeIcon icon={faBuilding} />
+                                <p>Credence Analytics</p>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faLocation} />
+                                <p>Malad, Mumbai</p>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faCalendarDays} />
+                                <p>May 2023 - Present</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="education-cont">
+                    <h1>Education</h1>
+                    <div className="experience">
+                        <div className="upper-section">
+                            <h3>Bachelor of Arts in Political Science & Psychology</h3>
+                            {/* <span className="tenure">Full Time</span> */}
+                        </div>
+                        <div className="lower-section">
+                            <span>
+                                <FontAwesomeIcon icon={faBuilding} />
+                                <p>Gokhale Education Society(Mumbai University)</p>
+                            </span>
+                            {/* <span>
+                                <FontAwesomeIcon icon={faLocation} />
+                                <p>Borivali, Mumbai</p>
+                            </span> */}
+                            <span>
+                                <FontAwesomeIcon icon={faCalendarDays} />
+                                <p>June 2019 - April 2022</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
             <div></div>
             <hr />
             <div className="footer-links">
-                <ul>
+                {/* <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Technologies</a></li>
                     <li><a href="#">Projects</a></li>
                     <li><a href="#">Contact</a></li>
-                </ul>
-                <p>Build on Design by Pavan MG</p>
+                </ul> */}
+                <p>Built on Design by Pavan MG</p>
             </div>
         </footer>
     )
