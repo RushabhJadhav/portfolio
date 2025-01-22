e7317c839924f5c804157c0e24ad521f35265abf
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-22 22:50:37 +0530

    added typing animation on intro text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,12 @@
 const Intro = () => {
     return (
         <div className="intro">
-            <h1>Hi, I'm Rushabh, a Self Taught <br/> Frontend Developer</h1>
-            <span>Based in Mumbai, Maharashtra, India <br/> Specialized in Vue and React</span>
-            {/* <div className="picture"></div> */}
-            {/* <button>Resume</button> */}
+            <div className="intro-text">
+                <h1 className="typed">Hi, I'm Rushabh, a Self Taught <br/> Frontend Developer</h1>
+                <span>Based in Mumbai, Maharashtra, India <br/> Specialized in Vue and React</span>
+                {/* <div className="picture"></div> */}
+                {/* <button>Resume</button> */}
+            </div>
         </div>
     )
 }
