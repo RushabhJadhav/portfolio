16af9332add86f7c3a7a821d7f943c97cf3b6535
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-23 21:50:49 +0530

    typing effect change

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,8 @@ const Intro = () => {
     return (
         <div className="intro">
             <div className="intro-text">
-                <h1 className="typed">Hi, I'm Rushabh, a Self Taught <br/> Frontend Developer</h1>
+                <h1 className="typed">Hi, I'm Rushabh, a Self Taught</h1>
+                <h1>Frontend Developer</h1>
                 <span>Based in Mumbai, Maharashtra, India <br/> Specialized in Vue and React</span>
                 {/* <div className="picture"></div> */}
                 {/* <button>Resume</button> */}
