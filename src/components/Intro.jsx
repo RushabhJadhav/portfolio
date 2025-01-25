00d7670ec7eedaae4a4c3fd27dc15bb12914a0bd
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-25 10:18:20 +0530

    added typing animation styles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
 const Intro = () => {
     return (
         <div className="intro">
             <div className="intro-text">
-                <h1 className="typed">Hi, I'm Rushabh, a Self Taught</h1>
+                <h1>Hi, I'm Rushabh, a Self Taught</h1>
                 <h1>Frontend Developer</h1>
-                <span>Based in Mumbai, Maharashtra, India <br/> Specialized in Vue and React</span>
-                {/* <div className="picture"></div> */}
-                {/* <button>Resume</button> */}
+                <span>Based in Mumbai, Maharashtra, India</span>
+                <span>Specialized in Vue and React</span>
+                {/* <a className="CV_btn" href="../assets/pdf/Rushabh_Jadhav_CV.pdf" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faDownload} />
+                    Resume
+                </a> */}
             </div>
         </div>
     )
