32a19e291476f8d1206e11416f13e4fd7d1748ae
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-30 18:55:34 +0530

    height on sections

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
 const Intro = () => {
     return (
         <div className="intro">
@@ -6,10 +9,10 @@ const Intro = () => {
                 <h1>Frontend Developer</h1>
                 <span>Based in Mumbai, Maharashtra, India</span>
                 <span>Specialized in Vue and React</span>
-                {/* <a className="CV_btn" href="../assets/pdf/Rushabh_Jadhav_CV.pdf" target="_blank" rel="noopener noreferrer">
+                <a className="CV_btn" href="../assets/pdf/Rushabh_Jadhav_CV.pdf" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faDownload} />
                     Resume
-                </a> */}
+                </a>
             </div>
         </div>
     )
