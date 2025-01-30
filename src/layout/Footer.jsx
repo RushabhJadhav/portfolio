a2666f340b0ee509eca470b6512d47136db86f5e
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-30 19:22:53 +0530

    height alignment

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,18 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <div></div>
-            <hr />
-            <div className="footer-links">
-                {/* <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Technologies</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul> */}
-                <p>Handcrafter with &#129505; by Rushabh &#169; 2025</p>
-            </div>
+            <p>Handcrafter with &#129505; by Rushabh &#169; 2025</p>
         </footer>
     )
 }
