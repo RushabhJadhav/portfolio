406f8971ae0c4097b9d8147a0e658b29ec73baa8
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2025-01-31 11:52:37 +0530

    added intersectionObserver

--- src/layout/PageBody.jsx
+++ src/layout/PageBody.jsx
@@ -1,8 +1,25 @@
 import Intro from "../components/Intro";
 import TechStack from "../components/TechStack";
 import Projects from "../components/Projects";
+import { useEffect } from "react";
 
 const PageBody = () => {
+    useEffect(() => {
+        const sections = document.querySelectorAll(".section")
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                entry.target.classList.toggle("show", entry.isIntersecting)
+            })
+        })
+
+        sections.forEach(section => observer.observe(section))
+
+        return () => {
+            sections.forEach(section => observer.unobserve(section));
+        };
+    }, [])
+
     return (
         <main>
             <Intro></Intro>
