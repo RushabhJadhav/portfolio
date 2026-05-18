188be2d34d8b77e1dc7d6823fa1205b0bed73729
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-05-18 12:07:09 +0530

    restructured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import Header from "./layout/Header";
 import PageBody from "./layout/PageBody";
 import Footer from "./layout/Footer";
-import About from "./components/About";
-import TechStack from "./components/TechStack";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<PageBody />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/tech-stack" element={<TechStack />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PageBody />} />
+        {/* Fallback to home for any other route */}
+        <Route path="*" element={<PageBody />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/layout/PageBody.jsx
+++ src/layout/PageBody.jsx
@@ -1,37 +1,19 @@
 import Intro from "../components/Intro";
 import TechStack from "../components/TechStack";
 import Projects from "../components/Projects";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import About from "../components/About";
+import Contact from "../components/Contact";
 
 const PageBody = () => {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-        const sections = document.querySelectorAll(".section")
-
-        if(pathname == "/") {
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    entry.target.classList.toggle("show", entry.isIntersecting)
-                })
-            })
-    
-            sections.forEach(section => observer.observe(section))
-    
-            return () => {
-                sections.forEach(section => observer.unobserve(section));
-            };
-        }
-    }, [])
-
     return (
         <main>
-            <Intro></Intro>
-            <TechStack></TechStack>
-            <Projects></Projects>
+            <Intro />
+            <About />
+            <TechStack />
+            <Projects />
+            <Contact />
         </main>
-    )
-}
+    );
+};
 
 export default PageBody;
