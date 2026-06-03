0c677074b9c7f0857c3a51a5f04055e692717c8c
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-06-03 10:58:41 +0530

    pagebody component changes

--- src/layout/PageBody.jsx
+++ src/layout/PageBody.jsx
@@ -1,9 +1,15 @@
 import Intro from "../components/Intro";
+import TechStack from "../components/TechStack";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 const PageBody = () => {
     return (
         <main>
             <Intro />
+            <TechStack />
+            <Projects />
+            <Contact />
         </main>
     );
 };
